10c39c480c270c14b739929d704c3fd1e40f333e
takuya-hasumi <t_hasumi@innovation.co.jp>
2018-04-27 09:41:10 +0900

    fix: 修正諸々

--- code/index.php
+++ code/index.php
@@ -10,7 +10,6 @@ $query  = $reqParams['query'];
 
 // BaseController, BaseModelの呼び出し
 require("./vendor/controller/BaseController.php");
-require("./vendor/model/Base.php");
 
 // 任意のコントローラを選択
 $class_name = $route->selectController($module);
@@ -22,9 +21,9 @@ try {
     $exec->Action();
     $stmt = $pdo->prepare("INSERT INTO commit_table (user_name) VALUES (:user_name)");
     $stmt->bindParam(':user_name', $env['DB_USERNAME'], PDO::PARAM_STR);
-    if (!$stmt->execute()) {
-        throw new Exception('テーブルの書き込みに失敗したZ');
-    }
+    // if (!$stmt->execute()) {
+    //     throw new Exception('テーブルの書き込みに失敗したZ');
+    // }
     $pdo->commit();
 
 } catch (PDOException $e) {
